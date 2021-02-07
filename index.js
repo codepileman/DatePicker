c3daded30a4f849d51f043384b836d0409beda89
Yang Xu <xydotguy@gmail.com>
2021-02-07 17:18:31 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,130 @@
+const date_picker_element = document.querySelector('.date-picker');
+const selected_date_element = document.querySelector('.date-picker .selected-date');
+const dates_element = document.querySelector('.date-picker .dates');
+const mth_element = document.querySelector('.date-picker .dates .month .mth');
+const next_mth_element = document.querySelector('.date-picker .dates .month .next-mth');
+const prev_mth_element = document.querySelector('.date-picker .dates .month .prev-mth');
+
+const days_element = document.querySelector('.date-picker .dates .days');
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+let date = new Date();
+let day = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+
+let selectedDate = date;
+let selectedDay = day;
+let selectedMonth = month;
+let selectedYear = year;
+
+mth_element.textContent = months[month] + ' ' + year;
+
+selected_date_element.textContent = formatDate(date);
+selected_date_element.dataset.value = selectedDate;
+
+
+//populate day, month and year when page initialized or when arrow clicked
+populateDates();
+
+date_picker_element.addEventListener('click', toggleDatePicker);
+next_mth_element.addEventListener('click', goToNextMonth);
+prev_mth_element.addEventListener('click', goToPrevMonth);
+
+function toggleDatePicker(e) {
+    if(!checkEventPathForClass(e.path, 'dates')){
+        dates_element.classList.toggle('active');
+    }
+} 
+
+function goToNextMonth(e){
+    month++;
+    if(month > 11){
+        month = 0;
+        year++;
+    }
+    mth_element.textContent = months[month] + ' ' + year;
+    populateDates(); 
+}
+
+function goToPrevMonth(e){
+    month--;
+    if(month < 0){
+        month = 11;
+        year--;
+    }
+    mth_element.textContent = months[month] + ' ' + year;
+    populateDates();
+    
+}
+
+function populateDates (e){
+    days_element.innerHTML = '';
+    let amount_days = 0;
+    if([0, 2, 4, 6, 7, 9, 11].includes(month)){
+        amount_days = 31;
+    }else if(month == 1 && year % 4 === 0){
+        amount_days = 29;
+    }else if(month == 1 && year % 4 !== 0){
+        amount_days = 28;
+    }else{
+        amount_days = 30;
+    }
+
+    for(let i = 0; i < amount_days; i++){
+        const day_element = document.createElement('div');
+        day_element.classList.add('day');
+        day_element.textContent = i + 1;
+
+        //if selected
+        if(selectedDay == (i + 1) && selectedMonth == month && selectedYear == year){
+            day_element.classList.add('selected');
+        }
+
+        day_element.addEventListener('click', () => {
+            selectedDate = new Date(year + '-' + (month + 1) + '-' + (i + 1));
+            selectedDay = i + 1;
+            selectedMonth = month;
+            selectedYear = year;
+
+            selected_date_element.textContent = formatDate(selectedDate);
+            selected_date_element.dataset.value = selectedDate;
+
+            populateDates();
+        });
+
+        days_element.appendChild(day_element);
+
+    }
+
+
+}
+
+
+//click on dates would not toggle the picker
+function checkEventPathForClass(path, selector){
+    for(let i = 0 ; i < path.length; i++){
+        if(path[i].classList && path[i].classList.contains(selector)){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function formatDate(d){
+    let day = d.getDate();
+    if(day < 10){
+        day = '0' + day;
+    }
+
+    let month = d.getMonth() + 1;
+    if(month < 10){
+        month = '0' + month;
+    }
+
+    let year = d.getFullYear();
+
+    return month + ' / ' + day + ' / ' + year;
+}
